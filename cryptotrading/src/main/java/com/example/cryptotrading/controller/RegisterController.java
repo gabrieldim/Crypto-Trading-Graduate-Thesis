c0cb8fa8508389526f6e193d5ed6befc682e3c8d
gabrieldim <dimitrievskig000@gmail.com>
2022-07-21 21:03:52 +0200

    register form completed

--- cryptotrading/src/main/java/com/example/cryptotrading/controller/RegisterController.java
+++ cryptotrading/src/main/java/com/example/cryptotrading/controller/RegisterController.java
@@ -3,6 +3,7 @@ package com.example.cryptotrading.controller;
 import com.example.cryptotrading.exceptions.InvalidUserCredentialsException;
 import com.example.cryptotrading.exceptions.InvalidUserPasswordsException;
 import com.example.cryptotrading.exceptions.UserAlreadyExsistsException;
+import com.example.cryptotrading.model.dto.RegisterUserDto;
 import com.example.cryptotrading.model.enumeration.Role;
 import com.example.cryptotrading.service.UserService;
 import org.springframework.web.bind.annotation.*;
@@ -19,15 +20,11 @@ public class RegisterController {
     }
 
     @PostMapping
-    public void register(@RequestParam String username,
-                           @RequestParam String password,
-                           @RequestParam String repeatPassword,
-                           @RequestParam String firstName,
-                           @RequestParam String lastName,
-                           @RequestParam Role role)
+    public void register(@RequestBody RegisterUserDto registerUserDto)
             throws UserAlreadyExsistsException, InvalidUserPasswordsException, InvalidUserCredentialsException {
         System.out.println("test");
-        this.userService.register(username, password, repeatPassword, firstName, lastName, role);
+        this.userService.register(registerUserDto.getUsername(), registerUserDto.getPassword(), registerUserDto.getRepeatPassword(),
+                registerUserDto.getFirstName(), registerUserDto.getLastName(), registerUserDto.getRole());
 
     }
 
---
+++ cryptotrading/src/main/java/com/example/cryptotrading/model/dto/RegisterUserDto.java
@@ -0,0 +1,18 @@
+package com.example.cryptotrading.model.dto;
+
+import com.example.cryptotrading.model.enumeration.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterUserDto {
+    private String username;
+    private String password;
+    private String repeatPassword;
+    private String firstName;
+    private String lastName;
+    private Role role;
+}
