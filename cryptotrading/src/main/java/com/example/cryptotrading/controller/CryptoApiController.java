b0965072ae1f46b8e435a82727b417dca3f8d037
gabrieldim <dimitrievskig000@gmail.com>
2022-07-30 13:04:32 +0200

    graph data limit and sorting

--- cryptotrading/src/main/java/com/example/cryptotrading/controller/CryptoApiController.java
+++ cryptotrading/src/main/java/com/example/cryptotrading/controller/CryptoApiController.java
@@ -9,16 +9,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -32,9 +32,15 @@ public class CryptoApiController {
     }
 
     @GetMapping("/crypto")
-    public List<CryptoHistoryGraphData> getCrypto(){
+    public List<CryptoHistoryGraphData> getCrypto(@RequestParam String cryptoSymbol){
+        System.out.println("Symbol to filter: " + cryptoSymbol);
+        List<CryptoHistoryGraphData> cryptoHistoryGraphData = cryptoHistoryGraphDataService.getHistoricalCryptoData()
+                .stream().filter(x -> x.getSymbol().equals(cryptoSymbol)).limit(13)
+                .collect(Collectors.toList());
 
-        return cryptoHistoryGraphDataService.getHistoricalCryptoData();
+        Collections.reverse(cryptoHistoryGraphData);
+
+        return cryptoHistoryGraphData;
     }
 
     @GetMapping("/cryptoSymbolName")
